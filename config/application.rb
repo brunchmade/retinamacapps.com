45ce4d674cd428b032c135acac85f02cbcf3fc47
Wells Riley <wells.riley@gmail.com>
2012-08-18 14:43:30 -0700

    removed config thing

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-config.middleware.use Rack::StaticCache, 
-  urls: %w(
-    /images
-    /favicon.ico
-  ),
-  root: "public_cached"
+# config.middleware.use Rack::StaticCache, 
+#   urls: %w(
+#     /images
+#     /favicon.ico
+#   ),
+#   root: "public_cached"
