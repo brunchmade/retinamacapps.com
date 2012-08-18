6c81ab2a70725ec8ae032ab7831daaa46b62519e
Wells Riley <wells.riley@gmail.com>
2012-08-18 14:17:08 -0700

    added caching stuff

--- config.rb
+++ config.rb
@@ -75,3 +75,11 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+config.middleware.use Rack::StaticCache, 
+  urls: %w(
+    /images
+    /robots.txt
+    /favicon.ico
+  ),
+root: "public_cached"
